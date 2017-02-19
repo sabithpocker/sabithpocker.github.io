0e96d39076d62a94050135fabc3c86bc9e73691b
Sabith Pocker <sabith@techolution.com>
2017-02-20 02:58:13 +0530

    spirograph added

--- js/main.js
+++ js/main.js
@@ -3,11 +3,35 @@ function intro() {
         height = window.innerHeight,
         count = Math.max(window.innerWidth, window.innerHeight) / 10;
     svg = createSVG();
-    $(document).on("click touchstart", explode);
+    //$(document).on("click touchstart", explode);
+    $('#controls input').on("input", updateSpirograph);
     lessPoly(svg, count, width / 2, height / 2);
     //createFloor(setup.vertices);
+    updateValues();
+    updateSpirograph();
+    //createSpirograph('vis', 60,59,58, 500, "rgba(255,255,255,1)", 0.5, "none");
+    //createSpirograph('vis', 60,5,2, 10, "rgba(255,0,255,1)", 0.3, "rgba(25,255,255,.8)");
+    //createSpirograph('vis', 60,25,12, 40, "rgba(255,255,255,1)", 1, "rgba(25,25,255,.4)");
+
 }
 window.onload = intro;
+
+function updateSpirograph(){
+    var r = +$('#innerR').val();
+    var R = +$('#outerR').val();
+    var p = +$('#locus').val();
+    var count = +$('#count').val();
+    updateValues();
+    createSpirograph('vis', r,R,p,count, "rgba(0,0,0,0.4)", 1, "none");
+}
+
+function updateValues(){
+    $('#innerRVal').text($('#innerR').val());
+    $('#outerRVal').text($('#outerR').val());
+    $('#locusVal').text($('#locus').val());
+    $('#countVal').text($('#count').val());   
+}
+
 
 var setup = {};
 
@@ -16,7 +40,10 @@ function lessPoly(svg, count, width, height) {
 
     var triangles = getTrianglesFromVertices(vertices);
 
-    var polygons = getPolygonsFromVertices(vertices, [[0,0],[width, height]]);
+    var polygons = getPolygonsFromVertices(vertices, [
+        [0, 0],
+        [width, height]
+    ]);
 
 
 
@@ -56,11 +83,14 @@ function lessPoly(svg, count, width, height) {
         .enter()
         .append("circle")
         .attr("cx", function(d) {
-            return d[0]; })
+            return d[0];
+        })
         .attr("cy", function(d) {
-            return d[1]; })
+            return d[1];
+        })
         .attr("r", function(d) {
-            return 5; })
+            return 5;
+        })
         .attr("stroke", "white")
         .attr("stroke-width", 1)
         .attr("fill", "transparent");
@@ -95,9 +125,11 @@ function getPolygonsFromVertices(vertices, extent) {
     voronoi.extent(extent);
     return voronoi(vertices).polygons();
 }
+
 function rndPosNeg() {
     return (Math.random() * 2) - 1;
 }
+
 function explode() {
     var tl = new TimelineMax({ repeat: 1, yoyo: true });
 
@@ -112,4 +144,41 @@ function explode() {
             transformOrigin: "center center"
         }, .5);
     });
+}
+function createSpirograph(id, R, r, p, reps, stroke, strokeWidth, fill) {
+    $('#'+id).remove();
+    var s = document.createElementNS(d3.namespaces.svg, 'svg');
+    s.id = id;
+    $('.content').append(s);
+    s = d3.select('#'+id)
+     .attr('width', '100%')
+     .attr('height', '100%');
+
+    var t = 0; // increment
+    var w = $('#'+id).width();
+    var h = $('#'+id).height();
+
+    var lineFunction = d3.line()
+        .x(function(d) {
+            return x(d) + w / 2 })
+        .y(function(d) {
+            return y(d) + h / 2 });
+
+    s.append("path")
+        .attr("d", lineFunction(d3.range(0, reps, 0.01)))
+        .attr("stroke", stroke)
+        .attr("stroke-width", strokeWidth)
+        .attr("fill", fill);
+
+
+    // parametric equations
+    //x(t)=(R+r)cos(t) + p*cos((R+r)t/r) 
+    function x(t) {
+        return ((R + r) * Math.cos(t)) + (p * Math.cos((R + r) * (t / r)))
+    }
+
+    //y(t)=(R+r)sin(t) + p*sin((R+r)t/r)
+    function y(t) {
+        return ((R + r) * Math.sin(t)) + (p * Math.sin((R + r) * (t / r)))
+    }
 }
