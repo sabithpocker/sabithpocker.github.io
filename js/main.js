1bd16335b57690966377f9b9c14559a8d05d830b
Sabith Pocker <sabithpocker@gmail.com>
2019-05-18 06:11:59 +0530

    Animate multiple fields
    Trying to animate r and p

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
 function init() {
   const spirograph = document.querySelector("mm-spirograph");
   const r = parseFloat(spirograph.getAttribute("moving-circle-radius"));
-  animateSpirograph(spirograph, r, 0.0001 * window.devicePixelRatio);
+  const p = parseFloat(spirograph.getAttribute("moving-circle-locus-length"));
+  animateSpirograph(
+    spirograph,
+    r,
+    p,
+    0.0001 * window.devicePixelRatio,
+    0.1 * window.devicePixelRatio
+  );
 }
 window.onload = init;
 
-function animateSpirograph(spirograph, r, increment) {
+function animateSpirograph(spirograph, r, p, rIncrement, pIncrement) {
   let v = r;
-  if (v + increment > 100) increment = -Math.abs(increment);
-  if (v + increment <= 0) increment = Math.abs(increment);
-  v = v + increment;
+  if (v + rIncrement > 100) rIncrement = -Math.abs(rIncrement);
+  if (v + rIncrement <= 0) rIncrement = Math.abs(rIncrement);
+  v = v + rIncrement;
   r = v;
+  let w = p;
+  if (w + pIncrement > 100) pIncrement = -Math.abs(pIncrement);
+  if (w + pIncrement <= 0) pIncrement = Math.abs(pIncrement);
+  w = w + pIncrement;
+  p = w;
   spirograph.setAttribute("moving-circle-radius", r);
+  spirograph.setAttribute("moving-circle-locus-length", p);
   window.requestAnimationFrame(() =>
-    animateSpirograph(spirograph, r, increment)
+    animateSpirograph(spirograph, r, p, rIncrement, pIncrement)
   );
 }
