3222f93204a5d30434052a110e1093f88f7ebffd
Sabith Pocker <sabithpocker@gmail.com>
2021-05-21 23:10:30 +0530

    More changes

---
+++ sample.js
@@ -0,0 +1,71 @@
+
+const data = [{
+    language: 'Spring Boot',
+    ram: 1,
+    cpu: 1
+},
+{
+    language: 'Node JS',
+    ram: 1,
+    cpu: 1
+},
+{
+    language: 'WordPress',
+    ram: 2,
+    cpu: 1
+},
+{
+    language: 'Go',
+    ram: 2,
+    cpu: 1
+},
+{
+    language: 'Nginx',
+    ram: 1,
+    cpu: 1
+},
+{
+    language: 'php',
+    ram: 1,
+    cpu: 3
+},
+{
+    language: 'angular / react / view',
+    ram: 2,
+    cpu: 1
+},
+{
+    language: 'python',
+    ram: 1,
+    cpu: 1
+},
+{
+    language: 'java war',
+    ram: 1,
+    cpu: 1
+},
+{
+    language: 'apache',
+    ram: 1,
+    cpu: 1
+},
+{
+    language: 'tomcat',
+    ram: 1,
+    cpu: 2
+},
+{
+    language: 'ruby',
+    ram: 1,
+    cpu: 1
+}];
+
+const pricing = [{
+    item: 'ram',
+    qty: 1,
+    price: 200
+}, {
+    item: 'cpu',
+    qty: 1,
+    price: 500
+}]
